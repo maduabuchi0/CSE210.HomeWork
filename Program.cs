d3727631b5ad810c835cb07fdad09bdd10b4a92a
maduabuchi0 <motu@byupathway.edu>
2025-05-13 21:52:29 +0100

    CSE210.HomeWork

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.Write("what is your first name");
+        string first = Console.ReadLine();
+
+        Console.Write("what is your last name");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"your is {last},{first} {last}.");
+    }
+}
