31476b201f1942c6a15971027e2d2642b151cf50
maduabuchi0 <motu@byupathway.edu>
2025-05-13 22:12:12 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,51 @@
 using System;
 
-
 class Program
 {
     static void Main(string[] args)
-
     {
-        Console.Write("what is your first name");
-        string first = Console.ReadLine();
+        string playAgain;
 
-        Console.Write("what is your last name");
-        string last = Console.ReadLine();
+        do
+        {
+            // Generate a random magic number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
 
-        Console.WriteLine($"your is {last},{first} {last}.");
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Guess My Number Game!");
+
+            // Loop until the user guesses the magic number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask the user if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
