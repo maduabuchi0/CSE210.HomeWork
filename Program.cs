b8664064e6872343770f0c91959ea3a019b14725
maduabuchi0 <motu@byupathway.edu>
2025-05-13 22:10:33 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
 using System;
 
-
 class Program
 {
     static void Main(string[] args)
-
     {
-        Console.Write("what is your first name");
-        string first = Console.ReadLine();
+        DisplayWelcome();
 
-        Console.Write("what is your last name");
-        string last = Console.ReadLine();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
 
-        Console.WriteLine($"your is {last},{first} {last}.");
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
